21876abf9b669ebd5e381a80ff9080fc71690bf1
CharlieHuang <notwist123@gmail.com>
2019-03-01 23:50:18 +0800

    Add unit test[f(4)=5(should be wrong)]

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 function fibonacci(number) {
-    return number;
+    if (number === 1 || number === 0)
+        return number;
+    
+    return fibonacci(number-1) + fibonacci(number-2);
 }
 module.exports = fibonacci;
--- test/fibonacci.test.js
+++ test/fibonacci.test.js
@@ -2,8 +2,17 @@ const fibonacci = require('../index');
 var assert = require('assert');
 
 
-describe('Unit Test for Fibonacci', ()=> {
-  it('Test f(1) = 1', ()=> {
+describe('For testing the correct Value', ()=> {
+    it('Test f(1) should be 1', ()=> {
     assert.equal(fibonacci(1), 1);
-  });
+    });
+    it('Test f(4) should be 3', ()=> {
+    assert.equal(fibonacci(4), 3);
+    });
+});
+
+describe('For testing the wrong Value', ()=> {
+    it('Test f(4) should not be 5', ()=> {
+        assert.notEqual(fibonacci(4), 5);
+    });
 });
