bb623053d82edbbdca032129a4538053a5e105b3
CharlieHuang <notwist123@gmail.com>
2019-03-02 00:06:14 +0800

    Add unit test [f(999) = f(998) + f(997)], and modify the code to let it pass

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+var temp = [];
 function fibonacci(number) {
+    if (temp[number]) 
+        return temp[number];
     if (number === 1 || number === 0)
         return number;
     
-    return fibonacci(number-1) + fibonacci(number-2);
+    return temp[number] = fibonacci(number-1) + fibonacci(number-2);
 }
 module.exports = fibonacci;
--- test/fibonacci.test.js
+++ test/fibonacci.test.js
@@ -4,10 +4,13 @@ var assert = require('assert');
 
 describe('For testing the correct Value', ()=> {
     it('Test f(1) should be 1', ()=> {
-    assert.equal(fibonacci(1), 1);
+        assert.equal(fibonacci(1), 1);
     });
     it('Test f(4) should be 3', ()=> {
-    assert.equal(fibonacci(4), 3);
+        assert.equal(fibonacci(4), 3);
+    });
+    it('Test f(999) should be f(998)+f(997)', ()=> {
+        assert.equal(fibonacci(999), fibonacci(998) + fibonacci(997));
     });
 });
 
